864843b98e55476805d8093d0707be7dedb326ba
William Zhang <wzha0123@student.monash.edu>
2021-02-11 16:39:00 +1100

    Added validation

--- MathsProject-hyperCubeWebsite/website/dieHard.js
+++ MathsProject-hyperCubeWebsite/website/dieHard.js
@@ -10,6 +10,19 @@ SUBMIT.click(function (e) {
     seeCords = SHOW_COORDS.is(":checked");
     water_target = bignumber(TARGET.val());
     e.preventDefault();
-    
+
+    function validate(val, control) {
+        if (math.smallerEq(val, 0)) {
+            control.addClass("is-invalid");
+            return false;
+        } else {
+            control.removeClass("is-invalid");
+            return true;
+        }
+    }
+    let is_valid = validate(jugX, WIDTH_INPUT) && validate(jugY, HEIGHT_INPUT) && validate(water_target, TARGET);
+    if (!is_valid) {
+        return;
+    }
     resetBoard();
 });
