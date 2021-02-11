935fe76868cdb8405a5b5981c4b0a11e20789a3d
William Zhang <wzha0123@student.monash.edu>
2021-02-11 17:29:23 +1100

    Auto stash before revert of "Merge branch 'main' into hyperCubeSite"

---
+++ MathsProject-hyperCubeWebsite/website/dieHard.js
@@ -0,0 +1,36 @@
+const WIDTH_INPUT = $("#table-width");
+const HEIGHT_INPUT = $("#table-height");
+const SHOW_COORDS = $("#show-coords");
+const TARGET = $("#target");
+const SUBMIT = $("#submit");
+
+function update(e) {
+    jugX = parseFloat(WIDTH_INPUT.val());
+    jugY = parseFloat(HEIGHT_INPUT.val());
+    seeCords = SHOW_COORDS.is(":checked");
+    water_target = bignumber(TARGET.val());
+    e.preventDefault();
+
+    function validate(val, control) {
+        if (math.smallerEq(val, 0)) {
+            control.addClass("is-invalid");
+            return false;
+        } else {
+            control.removeClass("is-invalid");
+            return true;
+        }
+    }
+    let is_valid = validate(jugX, WIDTH_INPUT) && validate(jugY, HEIGHT_INPUT) && validate(water_target, TARGET);
+    if (!is_valid) {
+        return;
+    }
+    resetBoard();
+}
+
+let inputs = [WIDTH_INPUT, HEIGHT_INPUT, SHOW_COORDS, TARGET];
+
+for (let input of inputs) {
+    input.change(update);
+}
+
+SUBMIT.click(update);
