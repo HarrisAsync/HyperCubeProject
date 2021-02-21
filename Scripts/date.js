da3b5b0b50d18917deeb88ef9d510bd16ee8c1ab
HarrisAsync <42229360+HarrisAsync@users.noreply.github.com>
2021-02-21 22:05:50 +1100

    Update date.js

--- Scripts/date.js
+++ Scripts/date.js
@@ -1,9 +1,9 @@
 
 // Resize the Buy me a coffee button if width gets too small ON START
 if (window.screen.availWidth < 417) {
-    document.getElementById("coffeeLink").src = "https://img.buymeacoffee.com/button-api/?text= &emoji=&slug=HypercubeSite&button_colour=c4ffe2&font_colour=000000&font_family=Arial&outline_colour=000000&coffee_colour=FFDD00";
+    document.getElementById("coffeeLink").src = "https://img.buymeacoffee.com/button-api/?text= &emoji=&slug=HypercubeSite&button_colour=40DCA5&font_colour=ffffff&font_family=Arial&outline_colour=000000&coffee_colour=FFDD00";
 } else {
-    document.getElementById("coffeeLink").src = "https://img.buymeacoffee.com/button-api/?text=Buy us a coffee&emoji=&slug=HypercubeSite&button_colour=c4ffe2&font_colour=000000&font_family=Arial&outline_colour=000000&coffee_colour=FFDD00";
+    document.getElementById("coffeeLink").src = "https://img.buymeacoffee.com/button-api/?text=Buy us a coffee&emoji=&slug=HypercubeSite&button_colour=40DCA5&font_colour=ffffff&font_family=Arial&outline_colour=000000&coffee_colour=FFDD00";
 }
 
 const COPYRIGHT_DATE = $("#copyright-date");
