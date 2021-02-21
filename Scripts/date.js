c4076a9a340df436f32b8d488dd05020933efd5d
HarrisAsync <42229360+HarrisAsync@users.noreply.github.com>
2021-02-21 20:05:33 +1100

    coffee responsiveness updates

--- Scripts/date.js
+++ Scripts/date.js
@@ -2,6 +2,8 @@
 // Resize the Buy me a coffee button if width gets too small ON START
 if (window.screen.availWidth < 417) {
     document.getElementById("coffeeLink").src = "https://img.buymeacoffee.com/button-api/?text= &emoji=&slug=HypercubeSite&button_colour=c4ffe2&font_colour=000000&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00";
+} else {
+    document.getElementById("coffeeLink").src = "https://img.buymeacoffee.com/button-api/?text=Buy us a coffee &emoji=&slug=HypercubeSite&button_colour=c4ffe2&font_colour=000000&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00";
 }
 
 const COPYRIGHT_DATE = $("#copyright-date");
